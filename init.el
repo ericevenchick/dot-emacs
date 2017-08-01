49dbf73646d529e745034ae708cf5a056500aff8
Eric Evenchick <eric@evenchick.com>
2017-08-01 08:20:38 -0400

    package-initialize in init.el

--- init.el
+++ init.el
@@ -1,9 +1,14 @@
 ;; minimal init.el for loading config from org file
 ;; requires emacs 24+
 
-;; package.el adds this, but we'll initialize it later
-;(package-initialize)
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 
 (org-babel-load-file
 (expand-file-name "config.org"
 		  user-emacs-directory))
+
+;(load-file "~/.emacs.d/config.el")
